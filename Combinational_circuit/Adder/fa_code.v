fcba0e5ef3ba5a34cee6ceb4c1b7edd43dfac72e
dhanu-0910 <dhanushiya952@gmail.com>
2026-02-05 10:46:17 +0530

    Update fa_code.v

--- Combinational_circuit/Adder/fa_code.v
+++ Combinational_circuit/Adder/fa_code.v
@@ -1,10 +1,4 @@
-module full_adder(
-    input a,
-    input b,
-    input cin,
-    output sum,
-    output cout
-    );
-    assign sum = a^b^cin;
-    assign cout = (a&b) | (b&cin) | (a&cin);
+module full_adder(input a,b,c, output s,cout);
+assign s=a^b^c;
+assign cout=(a&b)|(b&c)|(a&c);
 endmodule
