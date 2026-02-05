d8376b0962d89f10cb520e492e0b62ab6ab6ea10
dhanu-0910 <dhanushiya952@gmail.com>
2026-02-05 10:48:27 +0530

    Update ha_code.v

--- Combinational_circuit/Adder/ha_code.v
+++ Combinational_circuit/Adder/ha_code.v
@@ -1,6 +1,4 @@
-module half_adder(a,b,sum,carry);
-    input a, b;
-    output sum, carry;
-    xor(sum, a, b);
-    and(carry, a, b);
+module half_adder(input a,b, output s,c);
+assign s=a^b;
+assign c=a&b;
 endmodule
