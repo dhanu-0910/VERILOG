3f3b5fd7ec0888fdbf70efe97d94ddb103a67cf4
dhanu-0910 <dhanushiya952@gmail.com>
2026-02-05 10:47:45 +0530

    Update ha_tb.v

--- Combinational_circuit/Adder/ha_tb.v
+++ Combinational_circuit/Adder/ha_tb.v
@@ -1,14 +1,17 @@
 module half_adder_tb;
 reg a,b;
-wire sum,carry;
-half_adder uut(.a(a),.b(b),.sum(sum),.carry(carry));
-    initial begin
-        $display("A B | S C");
-        $monitor("%b %b | %b %b",a,b,sum,carry);
-        a=0;b=0;#10;
-        a=0;b=1;#10;
-        a=1;b=0;#10;
-        a=1;b=1;#10;
-        $finish;
-    end
+wire s,c;
+half_adder DUT(.a(a),.b(b),.s(s),.c(c));
+initial begin
+a=0;b=0; #10;
+a=0;b=1; #10;
+a=1;b=0; #10;
+a=1;b=1; #10;
+$finish;
+end
+initial begin
+        $dumpfile("out_half_adder.vcd");
+        $dumpvars(0,half_adder_tb);
+$monitor("a=%b b=%b | s=%b c=%b", a,b,s,c);
+end
 endmodule
