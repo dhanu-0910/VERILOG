ddb3ea88071d0c724b2690850a75dd8265936a6b
dhanu-0910 <dhanushiya952@gmail.com>
2026-02-06 23:22:09 +0530

    Update 8:1mux suing 2:1.v

--- Combinational_circuit/Multiplexer/8:1mux suing 2:1.v
+++ Combinational_circuit/Multiplexer/8:1mux suing 2:1.v
@@ -8,7 +8,7 @@ mux_2to1 m0(d[1:0],s[0],y0);
 mux_2to1 m1(d[3:2],s[0],y1);
 mux_2to1 m2(d[5:4],s[0],y2);
 mux_2to1 m3(d[7:6],s[0],y3);
-mux_2to1 m4({y0,y1},s[1],y4);
-mux_2to1 m5({y2,y3},s[1],y5);
-mux_2to1 m6({y4,y5},s[2],y);
+mux_2to1 m4({y1,y0},s[1],y4);
+mux_2to1 m5({y3,y2},s[1],y5);
+mux_2to1 m6({y5,y4},s[2],y);
 endmodule
