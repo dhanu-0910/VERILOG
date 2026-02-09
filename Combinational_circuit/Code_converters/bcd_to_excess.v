aa125e02ef05eba4fc8fd813c6c3696e1c240f57
dhanu-0910 <dhanushiya952@gmail.com>
2026-02-09 16:02:57 +0530

    Update bcd_to_excess.v

--- Combinational_circuit/Code_converters/bcd_to_excess.v
+++ Combinational_circuit/Code_converters/bcd_to_excess.v
@@ -2,7 +2,10 @@ module bcd_to_excess #(parameter n=1)(input [4*n-1:0]bcd, output reg [4*n-1:0]ex
 integer i;
 always @(*) begin
     for(i=0;i<n;i=i+1) begin
-        excess[4*i +: 4] = bcd[4*i +: 4] + 4'd3;
+        if(bcd[4*i +: 4]<=4'd9)
+            excess[4*i +: 4] = bcd[4*i +: 4] + 4'd3;
+        else
+            excess[4*i +: 4] = 4'd0;
     end
 end
 endmodule
