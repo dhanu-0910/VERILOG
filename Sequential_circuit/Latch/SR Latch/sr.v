021ffa01d3edfef745838bc6f02cc6c9f68b47a5
dhanu-0910 <dhanushiya952@gmail.com>
2026-02-11 15:22:35 +0530

    Update sr.v

--- Sequential_circuit/Latch/SR Latch/sr.v
+++ Sequential_circuit/Latch/SR Latch/sr.v
@@ -1,20 +1,16 @@
-module srlatch (input s,input r,output reg q,output reg qbar);
-always @(*) begin
+module srlatch (input s,input r,input clk,output reg q);
+always @(clk or s or r or q) begin
 if (s==1 && r==0) begin
     q=1;   
-    qbar=0;
 end
 else if (s==0 && r==1) begin
-    q=0;      
-    qbar=1;
+    q=0;     
 end
 else if (s==0 && r==0) begin
     q=q;
-    qbar=qbar;
 end
 else begin
-    q=0;
-    qbar=0;
+    q=1'bx;
 end
 end
 endmodule
