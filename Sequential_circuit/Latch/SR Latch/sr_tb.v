caa41cb63046c8e7ce544c846daa1392c6491e69
dhanu-0910 <dhanushiya952@gmail.com>
2026-02-11 15:23:05 +0530

    Update sr_tb.v

--- Sequential_circuit/Latch/SR Latch/sr_tb.v
+++ Sequential_circuit/Latch/SR Latch/sr_tb.v
@@ -1,13 +1,15 @@
 module srlatch_tb;
-reg s,r;
-wire q,qbar;
+reg s,r,clk;
+wire q;
 srlatch dut(.*);
 initial begin
     $dumpfile("out_srlatch.vcd");
     $dumpvars(0,srlatch_tb);
-    $monitor("time=%0t | s=%b r=%b | q=%b qbar=%b",$time,s,r,q,qbar);
+    $monitor("time=%0t clk=%b | s=%b r=%b | q=%b",$time,clk,s,r,q);
 end
 initial begin
+    clk=1;
+    s=0;r=1;#10;
     s=0;r=0;#10;
     s=0;r=1;#10;
     s=1;r=0;#10;
