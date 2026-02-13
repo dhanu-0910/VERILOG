5f166e7f7277643c6aff0d1a6ace6ca23b1352e6
dhanu-0910 <dhanushiya952@gmail.com>
2026-02-13 15:48:03 +0530

    Update siso_d.v

--- Sequential_circuit/register/siso_d.v
+++ Sequential_circuit/register/siso_d.v
@@ -1,23 +1,16 @@
-module siso (
-    input clk,rst,
-    input si,
-    output reg so
-);
-
-    reg [3:0] q;
-
-  always @(posedge clk) begin
-    if(rst)begin
-      q<=4'b0000;
-    end
-    else begin
-      q <= {si,q[3:1]};
-      so <= q[0];
-    end
+module siso (input clk,rst,input si,output reg so);
+reg [3:0] q;
+always @(posedge clk) begin
+  if(rst)begin
+    q<=4'b0000;
+    so<=1'b0;
   end
-    
+  else begin
+    q <= {si,q[3:1]};
+    so <= q[0];
+  end
+end
 endmodule
-
 
 // Code your testbench here
 // or browse Examples
@@ -28,15 +21,18 @@ module siso_tb;
   initial begin
     $monitor("t=%0t|si=%b clk=%b rst=%b|so=%b",$time,si,clk,rst,so);
     clk=0;
-    si=0;rst=1;
-    #5 si=1;rst=0;
-    #25 si=0;rst=0;
-    #45 si=1;rst=0;
+    rst=1;
+    si=1'b0;#10;
+    rst=0;
+    si=1'b1;#10;
+    si=1'b0;#10;
+    si=1'b0;#10;
+    si=1'b1;#10;
   end
   always #5 clk=~clk;
   initial begin
-    $dumpfile("tvk.vcd");
-    $dumpvars;
-    #400 $finish;
+    $dumpfile("out_siso.vcd");
+    $dumpvars(0,siso_tb);
+    #100 $finish;
   end
 endmodule
