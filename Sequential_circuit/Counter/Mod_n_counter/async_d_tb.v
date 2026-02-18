eab5bedcaedeae74d1a458341417ea46e6f06133
dhanu-0910 <dhanushiya952@gmail.com>
2026-02-18 15:19:22 +0530

    Create async_d_tb.v

---
+++ Sequential_circuit/Counter/Mod_n_counter/async_d_tb.v
@@ -0,0 +1,29 @@
+module mod_counter #(parameter n=4)(input clk,rst,output reg [n-1:0]q);
+  always @(posedge clk or posedge rst) begin
+    if(rst)
+      q<=0;
+    else if(q==n-1)
+      q<=0;
+    else
+      q=q+1;
+  end
+endmodule
+//Testbench
+module mod_counter_tb;
+parameter n=4;
+reg clk,rst;
+wire [n-1:0]q;
+mod_counter #(n) dut(.*);
+initial begin
+$dumpfile("out_mod_counter.vcd");
+  $dumpvars(1,mod_counter_tb);
+  $monitor("time=%0t clk=%b rst=%b q=%d",$time,clk,rst,q);
+end
+always #5 clk=~clk;
+initial begin
+        clk=1;
+rst=1;#10;
+rst=0;#200;
+$finish;
+end
+endmodule
