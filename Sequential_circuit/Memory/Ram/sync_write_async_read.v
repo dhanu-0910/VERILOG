764a2c4e5fb7adec11427db5a7f811330f9ff1d3
dhanu-0910 <dhanushiya952@gmail.com>
2026-02-22 15:07:27 +0530

    Create sync_write_async_read.v

---
+++ Sequential_circuit/Memory/Ram/sync_write_async_read.v
@@ -0,0 +1,11 @@
+module ram(input [7:0]data,input [15:0]addr,input clk,w_e,output [7:0]dout);
+  reg [7:0]mem[65535:0];
+  reg [5:0]reg_addr;
+  always @(posedge clk) begin
+    if(w_e)
+      mem[addr]<=data;
+    else
+      reg_addr<=addr;
+  end
+  assign dout=mem[reg_addr];
+endmodule
