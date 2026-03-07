5f4ef2617edab1886c00ac4cd65838c2577a3b23
dhanu-0910 <dhanushiya952@gmail.com>
2026-03-07 22:46:25 +0530

    Update sync_write_async_read.v

--- Sequential_circuit/Memory/Ram/sync_write_async_read.v
+++ Sequential_circuit/Memory/Ram/sync_write_async_read.v
@@ -1,11 +1,8 @@
-module ram(input [7:0]data,input [15:0]addr,input clk,w_e,output [7:0]dout);
-  reg [7:0]mem[65535:0];
-  reg [5:0]reg_addr;
+module ram(input [7:0]data,input [3:0]addr,input clk,w_e,output [7:0]dout);
+  reg [7:0]mem[15:0];
   always @(posedge clk) begin
     if(w_e)
       mem[addr]<=data;
-    else
-      reg_addr<=addr;
   end 
-  assign dout=mem[reg_addr];
+  assign dout=mem[addr];
 endmodule
